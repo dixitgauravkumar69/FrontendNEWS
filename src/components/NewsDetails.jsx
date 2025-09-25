c773aec1b86f5ed5a94314432607a551d1e7b44b
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2025-09-25 19:52:07 +0530

    Css error solve

--- src/components/NewsDetails.jsx
+++ src/components/NewsDetails.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./NewsDetail.css"; // create this CSS file
+import "./NewsDetail.css";
 
 export default function NewsDetail() {
   const { id } = useParams();
